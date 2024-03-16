ba70f64541acfa06268fc3b576b0b2d898da23de
Tim Zimmermann <tim@linux4.de>
2024-03-16 17:54:20 +0100

    universal9830: Kang AIDL RIL stack from a54x
    Change-Id: Id6ad2c9c52e7054d76121e53fb8d53df55c9b55e
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- universal9830-common-vendor.mk
+++ universal9830-common-vendor.mk
@@ -191,14 +191,20 @@ PRODUCT_PACKAGES += \
     sensors.grip \
     vendor.samsung.hardware.gnss@2.0 \
     vendor.samsung.hardware.gnss@2.1 \
-    vendor.samsung.hardware.radio.bridge@2.0 \
-    vendor.samsung.hardware.radio.bridge@2.1 \
-    vendor.samsung.hardware.radio.channel@2.0 \
+    vendor.samsung.hardware.radio-V1-ndk \
+    vendor.samsung.hardware.radio.bridge-V1-ndk \
+    vendor.samsung.hardware.radio.channel-V1-ndk \
+    vendor.samsung.hardware.radio.data-V1-ndk \
+    vendor.samsung.hardware.radio.messaging-V1-ndk \
+    vendor.samsung.hardware.radio.sim-V1-ndk \
     vendor.samsung.hardware.thermal@1.0 \
     vendor.samsung_slsi.hardware.eden_runtime@1.0-impl \
     vendor.samsung_slsi.hardware.eden_runtime@1.0 \
     com.android.hotwordenrollment.common.util \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
+    vendor.samsung.hardware.radio.exclude.slsi.xml \
+    vendor.samsung.hardware.radio_manifest_2_33.xml \
+    vendor.samsung.hardware.sehradio_manifest_2_33.xml \
     cass \
     cbd \
     android.hardware.drm@1.3-service.widevine \
